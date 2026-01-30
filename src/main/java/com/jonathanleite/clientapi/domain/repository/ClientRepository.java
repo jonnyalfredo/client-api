2dd817ae2c8b0a1d62287666b1df341a7fc49b52
JONATHAN ALFREDO LEITE <jhonatanleite0@gmail.com>
2026-01-30 17:20:18 -0300

    refactor: ajusta repositório para suportar filtros dinâmicos

--- src/main/java/com/jonathanleite/clientapi/domain/repository/ClientRepository.java
+++ src/main/java/com/jonathanleite/clientapi/domain/repository/ClientRepository.java
@@ -2,12 +2,15 @@ package com.jonathanleite.clientapi.domain.repository;
 
 import com.jonathanleite.clientapi.domain.entity.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface ClientRepository extends JpaRepository<Client, Long> {
+public interface ClientRepository extends
+        JpaRepository<Client, Long>,
+        JpaSpecificationExecutor<Client> {
 
     Optional<Client> findByEmail(String email);
 
